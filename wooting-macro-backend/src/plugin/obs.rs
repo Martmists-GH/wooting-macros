b0ea7b4e48a34e6b8ee772d380dc5b8039b83b68
medzernik <1900179+medzernik@users.noreply.github.com>
2024-05-07 12:42:00 +0200

    removed obs code and library - for now

--- wooting-macro-backend/src/plugin/obs.rs
+++ wooting-macro-backend/src/plugin/obs.rs
@@ -1,20 +1,6 @@
-use log::*;
-use obws::Client;
-
 /// Engage the OBS plugin.
 ///
 /// ! **UNIMPLEMENTED** - WIP already on a separate branch. We welcome suggestions of features.
 pub async fn engage_obs() {
-    // Connect to the OBS instance through obs-websocket.
-    let client = Client::connect("localhost", 4455, Some("password"))
-        .await
-        .unwrap();
-
-    // Get and print out version information of OBS and obs-websocket.
-    let version = client.general().version().await.unwrap();
-    info!("{:#?}", version);
-
-    // Get a list of available scenes and print them out.
-    let scene_list = client.scenes().list().await.unwrap();
-    info!("{:#?}", scene_list);
+    unimplemented!()
 }
