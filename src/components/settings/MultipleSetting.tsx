2dc7b7acfa67b77e9ec0eedb41f89f299431ead8
medzernik <1900179+medzernik@users.noreply.github.com>
2024-05-20 15:00:23 +0200

    basic framework for settings finished

--- src/components/settings/MultipleSetting.tsx
+++ src/components/settings/MultipleSetting.tsx
@@ -1,5 +1,14 @@
-import { Box, HStack, useRadio } from '@chakra-ui/react'
+import {
+  Box,
+  HStack,
+  VStack,
+  Text,
+  useRadio,
+  useRadioGroup,
+  useColorModeValue
+} from '@chakra-ui/react'
 import { MacroSettingOptionDefiniton } from '../../constants/enums'
+import { useMacroContext } from '../../contexts/macroContext'
 
 interface RadioCardProps {
   value: string
@@ -8,7 +17,61 @@ interface RadioCardProps {
   radio: any
 }
 
-export default function MultipleSetting({
+interface RadioBoxButtonProps {
+  title: string
+  description: string
+  onChange: (newValue: number) => void
+}
+
+export default function RadioBoxButton({
+  title,
+  description,
+  onChange
+}: RadioBoxButtonProps) {
+  const options = MacroSettingOptionDefiniton
+  const { macro, updateMacroRecordSeqDelay, getMacroRecordSeqDelayIndex } =
+    useMacroContext()
+
+  const { getRadioProps } = useRadioGroup({
+    name: 'framework',
+    defaultValue: 'react',
+    onChange: () => onChange
+  })
+
+  return (
+    <VStack w="full" spacing="0" textAlign="left">
+      <Text
+        as="b"
+        textAlign="start"
+        w="full"
+        fontSize="md"
+        fontWeight="semibold"
+      >
+        {title}
+      </Text>
+      <HStack spacing="2" w="full">
+        <Text fontSize="sm">{description}</Text>
+        <HStack marginEnd={3}>
+          {options.map((value, index) => {
+            const radio = getRadioProps({ value })
+            const index_macro = getMacroRecordSeqDelayIndex(macro)
+            return (
+              <MultipleSetting
+                key={value}
+                value={MacroSettingOptionDefiniton[index]}
+                selectedValue={index_macro}
+                onChange={updateMacroRecordSeqDelay}
+                radio={radio}
+              />
+            )
+          })}
+        </HStack>
+      </HStack>
+    </VStack>
+  )
+}
+
+function MultipleSetting({
   value,
   selectedValue,
   onChange,
@@ -19,6 +82,11 @@ export default function MultipleSetting({
     isChecked: value === MacroSettingOptionDefiniton[selectedValue],
     onChange: () => onChange(MacroSettingOptionDefiniton.indexOf(value))
   })
+
+  const kebabColour = useColorModeValue(
+    'primary-accent.200',
+    'primary-accent.200'
+  )
 
   const input = getInputProps()
   const checkbox = getRadioProps()
@@ -31,15 +99,18 @@ export default function MultipleSetting({
           {...checkbox}
           cursor="pointer"
           borderWidth="1px"
+          borderColor="gray"
           borderRadius="md"
-          boxShadow="md"
+          userSelect="none"
           _checked={{
-            bg: 'teal.600',
-            color: 'white',
-            borderColor: 'teal.600'
-          }}
-          _focus={{
-            boxShadow: 'outline'
+            textColor: 'black',
+            bg:
+              MacroSettingOptionDefiniton.indexOf(value) === 0
+                ? 'primary-accent.600'
+                : MacroSettingOptionDefiniton.indexOf(value) === 1
+                  ? 'red.600'
+                  : 'teal.600',
+            color: 'white'
           }}
           px={2}
           py={1}
