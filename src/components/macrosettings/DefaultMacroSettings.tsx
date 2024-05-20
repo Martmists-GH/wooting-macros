5957c3559f981cc85bc80b1a66a6d1388afb8467
medzernik <1900179+medzernik@users.noreply.github.com>
2024-05-20 15:00:23 +0200

    basic framework for settings finished

--- src/components/macrosettings/DefaultMacroSettings.tsx
+++ src/components/macrosettings/DefaultMacroSettings.tsx
@@ -1,52 +1,23 @@
-import { Divider, HStack, Text, useRadioGroup, VStack } from '@chakra-ui/react'
-import React from 'react'
-import MultipleSetting from '../settings/MultipleSetting'
-import { MacroSettingOptionDefiniton } from '../../constants/enums'
+import { Divider, Text, VStack } from '@chakra-ui/react'
 import { useMacroContext } from '../../contexts/macroContext'
+import RadioBoxButton from '../settings/MultipleSetting'
 
 export default function DefaultMacroSettings() {
-  const { macro, updateMacroRecordSeqDelay, getMacroRecordSeqDelayIndex } =
-    useMacroContext()
-
-  const options = MacroSettingOptionDefiniton
-
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: 'framework',
-    defaultValue: 'react',
-    onChange: () => updateMacroRecordSeqDelay
-  })
-
-  const group = getRootProps()
+  const { updateMacroRecordSeqDelay } = useMacroContext()
 
   return (
     <VStack w="full" spacing="4">
       <VStack w="full">
         <Text w="full" textStyle="miniHeader">
-          Window Settings
+          Macro recording settings
         </Text>
       </VStack>
       <VStack w="full" spacing={[4]}>
-        <HStack spacing={0}>
-          <Text as="b">Record delay when recording sequence</Text>
-          <HStack {...group}>
-            TODO: Move options.map inside the multiple settings and only give
-            options array,
-            {options.map((value, index) => {
-              const radio = getRadioProps({ value })
-              const index_macro = getMacroRecordSeqDelayIndex(macro)
-              return (
-                <MultipleSetting
-                  key={value}
-                  value={MacroSettingOptionDefiniton[index]}
-                  selectedValue={index_macro}
-                  onChange={updateMacroRecordSeqDelay}
-                  radio={radio}
-                />
-              )
-            })}
-          </HStack>
-        </HStack>
-
+        <RadioBoxButton
+          title="Record sequence delay"
+          description="Record delay when recording sequence"
+          onChange={updateMacroRecordSeqDelay}
+        />
         <Divider />
       </VStack>
     </VStack>
